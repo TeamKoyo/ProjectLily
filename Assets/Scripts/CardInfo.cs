908c7e667b593851a9dea24348a1123c7e517ec3
Freya <141486472+Frey-a@users.noreply.github.com>
2025-08-18 22:14:51 +0900

    Card
    카드 정보 스크립트 및 프리팹

---
+++ Assets/Scripts/CardInfo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardInfo : MonoBehaviour
+{
+    private CardData _cardInfo; // 정보
+
+    public int id;
+    public Image[] img;
+    public Text[] cost;
+    public Text cardName;
+    public Text description; // 효과 설명
+    #region Exclusive
+    public GameObject exclusive; // 전용 obj 부모
+    public Image charImg;
+    public Text charName;
+    #endregion
+
+    public void Init(CardData cardInfo)
+    {
+        _cardInfo = cardInfo;
+        id = _cardInfo.cardId;
+    }
+
+    private void SetCard()
+    {
+        // 이미지 등 자동 설정
+    }
+}
