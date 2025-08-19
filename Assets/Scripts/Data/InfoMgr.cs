6c3883c2f0bc82a378ea2351c2dc9eb52914861a
Freya <141486472+Frey-a@users.noreply.github.com>
2025-08-20 00:01:31 +0900

    Add Status
    status class 추가

--- Assets/Scripts/Data/InfoMgr.cs
+++ Assets/Scripts/Data/InfoMgr.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InfoMgr : MonoBehaviour
@@ -6,17 +7,57 @@ public class InfoMgr : MonoBehaviour
     public static InfoMgr Instance { get; private set; } // 읽기 전용 싱글톤 패턴
     public SODatabase database;
 
-    private void Awake() => Instance = this; // 싱글톤 초기화
+    private void Awake() // 싱글톤 초기화
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     #region Card
     public GameObject cardPrefab;
     #endregion
 
     #region Character
+    private int[] selectedCharId = new int[] { 0, 1, 2 };
     public GameObject charPrefab;
+    
+    public int[] getCharIds()
+    {
+        return selectedCharId;
+    }
     #endregion
 
     #region Enemy
     public GameObject enemyPrefab;
     #endregion
+
+    #region Status
+    private List<CharStatus> statuses = new List<CharStatus>();
+
+    public CharStatus LoadStatus(int charId)
+    {
+        CharStatus status = statuses.Find(state => state.charId == charId);
+
+        if(status != null)
+        {
+            return status;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    #endregion
+}
+
+public class CharStatus
+{
+    public int charId;
+    public int hp;
 }
